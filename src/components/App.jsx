bb6549d9d1b3c61b40da8252c61f2c919c6d17b0
Cameron Carruthers <cameronlcarruthers@gmail.com>
2020-08-26 17:31:42 -0600

    Add server routes returning hardcoded data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles, StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
 import axios from 'axios';
-// import JssProvider from 'react-jss';
 
-import Header from './Header.jsx';
-import Announcement from './Announcement.jsx';
-import Images from './Images.jsx';
-import Reviews from './Reviews.jsx';
-import Name from './Name.jsx';
-import StyleSelector from './StyleSelector.jsx';
-import Description from './Description.jsx';
-import Details from './Details.jsx';
+import Header from './Header';
+import Announcement from './Announcement';
+import Images from './Images';
+import Reviews from './Reviews';
+import Name from './Name';
+import StyleSelector from './StyleSelector';
+import Description from './Description';
+import Details from './Details';
 
-
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   grid: {
     width: '100%',
     margin: '0px',
     padding: '0px',
     display: 'flex',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   details: {
-    marginLeft: '20px'
+    marginLeft: '20px',
   },
   reviews: {
-    marginTop: "10px",
-    marginBottom: "10px"
+    marginTop: '10px',
+    marginBottom: '10px',
   },
   description: {
-    marginLeft: "50px",
-    marginTop: "20px"
+    marginLeft: '50px',
+    marginTop: '20px',
   },
   body: {
-    maxWidth: "85.375em",
-  }
+    maxWidth: '85.375em',
+  },
 }));
 
-// let ruleCounter = 0;
-
-// const createGenerateClassName = () => {
-//   return (rule, sheet) => {
-//     ruleCounter += 1;
-
-//     if (process.env.NODE_ENV === 'production') {
-//       return `c${ruleCounter}`;
-//     }
-
-//     if (sheet && sheet.options.meta) {
-//       return `${sheet.options.meta}-${rule.key}-${ruleCounter}`;
-//     }
-
-//     return `${rule.key}-${ruleCounter}`;
-//   };
-// }
-
-// const generateClassName = createGenerateClassName({
-//   productionPrefix: 'c',
-// })
-
-// const generateClassName = createGenerateClassName({
-//   disableGlobal: true,
-//   seed: 'p',
-//   productionPrefix: 'o'
-// });
-
 const App = (props) => {
-
   let productId;
   const classes = useStyles();
-  // const [productId, setProductId] = useState(null);
   const [currentProduct, setCurrentProduct] = useState(null);
   const [ratings, setRatings] = useState(null);
   const [styles, setStyles] = useState(null);
@@ -85,55 +54,61 @@ const App = (props) => {
 
     Promise.all([
       axios.get(`http://52.26.193.201:3000/reviews/${productId}/meta`),
-      axios.get(`http://52.26.193.201:3000/products/${productId}`),
-      axios.get(`http://52.26.193.201:3000/products/${productId}/styles`)
-  ])
-    .then(([resReviews, resProduct, resStyles]) => {
-      setRatings(resReviews.data.ratings);
-      setCurrentProduct(resProduct.data);
-      setStyles(resStyles.data.results);
-    })
-    .catch((err) => {console.log("axios get error: ", err)});
-  }, [productId])
+      axios.get(`/products/${productId}`),
+      axios.get(`/products/${productId}/styles`),
+    ])
+      .then(([resReviews, resProduct, resStyles]) => {
+        setRatings(resReviews.data.ratings);
+        setCurrentProduct(resProduct.data);
+        setStyles(resStyles.data.results);
+      })
+      .catch((err) => { console.log('axios get error: ', err); }); // eslint-disable-line no-console
+  }, [productId]);
 
   return (
-    // <StylesProvider generateClassName={generateClassName}>
-      <Grid container className={classes.grid}>
-        <Grid item xs={12}>
-          <Header id="header"/>
+    <Grid container className={classes.grid}>
+      <Grid item xs={12}>
+        <Header id="header" />
+      </Grid>
+      <Grid item xs={12}>
+        <Announcement id="announcement" />
+      </Grid>
+      <Grid container item xs={12} className={classes.body}>
+        <Grid item xs={7}>
+          <Images selectedStyle={selectedStyle} id="images" />
         </Grid>
-        <Grid item xs={12}>
-          <Announcement id="announcement"/>
+        <Grid container className={classes.details} item xs={4}>
+          <Grid item xs={12} className={classes.reviews}>
+            <Reviews productId={productId} ratings={ratings} id="reviews" />
+          </Grid>
+          <Grid item xs={12}>
+            <Name currentProduct={currentProduct} id="name" />
+          </Grid>
+          <Grid item xs={12}>
+            <StyleSelector styles={styles} currentProduct={currentProduct} selectedStyle={selectedStyle} setSelectedStyle={setSelectedStyle} id="styleSelector" />
+          </Grid>
         </Grid>
-        <Grid container item xs={12} className={classes.body}>
+        <Grid container item xs={12}>
           <Grid item xs={7}>
-            <Images selectedStyle={selectedStyle} id="images"/>
+            <div className={classes.description}>
+              <Description currentProduct={currentProduct} id="description" />
+            </div>
           </Grid>
-          <Grid container className={classes.details} item xs={4}>
-            <Grid item xs={12} className={classes.reviews}>
-              <Reviews productId={productId} ratings={ratings} id="reviews"/>
-            </Grid>
-            <Grid item xs={12}>
-              <Name currentProduct={currentProduct} id="name"/>
-            </Grid>
-            <Grid item xs={12}>
-              <StyleSelector styles={styles} currentProduct={currentProduct} selectedStyle={selectedStyle} setSelectedStyle={setSelectedStyle} id="styleSelector"/>
-            </Grid>
-          </Grid>
-          <Grid container item xs={12}>
-            <Grid item xs={7}>
-              <div className={classes.description}>
-                <Description currentProduct={currentProduct} id="description"/>
-              </div>
-            </Grid>
-            <Grid item xs={5}>
-              <Details currentProduct={currentProduct} id="details"/>
-            </Grid>
+          <Grid item xs={5}>
+            <Details currentProduct={currentProduct} id="details" />
           </Grid>
         </Grid>
       </Grid>
-    // </StylesProvider>
-  )
-}
+    </Grid>
+  );
+};
+
+App.defaultProps = {
+  productId: 1,
+};
+
+App.propTypes = {
+  productId: PropTypes.number,
+};
 
 export default App;
--- src/components/AppContainer.jsx
+++ src/components/AppContainer.jsx
@@ -1,28 +1,34 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
-import App from "./App.jsx";
+import PropTypes from 'prop-types';
+import App from './App';
 
 const generateClassName = createGenerateClassName({
   disableGlobal: true,
   seed: 'p',
-  productionPrefix: 'o'
+  productionPrefix: 'o',
 });
 
-const AppContainer = () => {
-  return (
-    <StylesProvider generateClassName={generateClassName}>
-      <Switch>
-        <Route exact path='/' component={App} />
-        <Route
-          path='/products/:productId'
-          render={props => {
-            const { productId } = props.match.params || {productId: 1};
-            return ( <App productId={productId}/>)
-          }} />
-      </Switch>
-    </StylesProvider>
-  );
+const AppContainer = () => (
+  <StylesProvider generateClassName={generateClassName}>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route
+        path="/:productId"
+        render={(props) => {
+          const { productId } = props.match.params || { productId: 1 };
+          return (<App productId={productId} />);
+        }}
+      />
+    </Switch>
+  </StylesProvider>
+);
+
+AppContainer.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default AppContainer;
