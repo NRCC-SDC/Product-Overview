3aad1e26ae6af0350138065af8c1ce7cfe707ab0
Cameron Carruthers <cameronlcarruthers@gmail.com>
2020-08-28 15:31:39 -0600

    Create functions to generate records and save to a json file

--- dataGenerator.js
+++ dataGenerator.js
@@ -5,7 +5,7 @@ const generateProduct = () => {
   const amountOfFeatures = Math.floor(Math.random() * 5);
   const features = [];
 
-  for (let i = 0; i < amountOfFeatures; i++) {
+  for (let i = 0; i < amountOfFeatures; i++) { // eslint-disable-line no-plusplus
     const feature = {};
     feature.feature = faker.commerce.productMaterial();
     feature.value = faker.commerce.productMaterial();
@@ -24,10 +24,21 @@ const generateProduct = () => {
   return product;
 };
 
-const saveData = () => {
-
+const generateProductData = (num) => {
+  const data = [];
+  for (let i = 0; i < num; i++) { // eslint-disable-line no-plusplus
+    const entry = generateProduct();
+    data.push(entry);
+  }
+  return data;
 };
 
-let result = generateProduct();
+const saveData = (numOfRecords) => {
+  const data = JSON.stringify(generateProductData(numOfRecords));
+  fs.writeFile('productData.json', data, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!'); // eslint-disable-line no-console
+  });
+};
 
-console.log(result);
+saveData(10);
